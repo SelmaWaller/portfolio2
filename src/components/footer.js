84c3cba1257730a0f8cc5a643286f8c8b39fe653
Selma Waller <selmawaller@gmail.com>
2020-04-02 00:15:45 +0200

    finished contact form, compressed images, added social media to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import linkedin from './../images/linkedin.png';
+import email from './../images/email.svg';
+import github from './../images/git.png';
+
 const Footer = () => {
   return (
     <div className="footer">
+      <a href="https://www.linkedin.com/in/selmawaller/" target="blank">
+        <img src={linkedin} alt="linkedin" title="LinkedIn" />
+      </a>
+      <a href="mailto:selmawaller@gmail.com">
+        <img src={email} alt="email" title="Send me an email" />
+      </a>
+      <a href="https://github.com/SelmaWaller" target="blank">
+        <img src={github} alt="github" title="Github" />
+      </a>
       <p>© 2020 selma waller</p>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+
 import ContentWave from './../components/page-title';
 
-function Contact() {
+const minMessageCount = 15;
+
+export default function Contact() {
   const [nameError, setNameError] = useState(true);
+  const [emailError, setEmailError] = useState(true);
+  const [messageError, setMessageError] = useState(true);
+  const [showResponse, setShowResponse] = useState(false);
+  const [count, setCount] = useState(minMessageCount);
+
+  let handleChange = input => {
+    let name = input.target.name;
+    let value = input.target.value;
+    let namePattern = /^[a-zA-ZæøåÆØÅ -]{1,}[ ]{1}[a-zA-ZæøåÆØÅ. -]{1,}$/;
+    let emailPattern = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+    let messagePattern = /^(.{15,})$/;
+
+    switch (name) {
+      case 'name':
+        namePattern.test(value) ? setNameError(false) : setNameError(true);
+        break;
+      case 'email':
+        emailPattern.test(value) ? setEmailError(false) : setEmailError(true);
+        break;
+      case 'message':
+        setCount(minMessageCount - value.length);
+        setMessageError(!messagePattern.test(value));
+        break;
+      default:
+        break;
+    }
+  };
+
+  let filterPhone = input => {
+    input.target.value = input.target.value.replace(/[^\d]/, '');
+  };
+
+  let submitForm = event => {
+    event.preventDefault();
+    setShowResponse(true);
+  };
 
   return (
     <>
@@ -15,42 +54,95 @@ function Contact() {
           <h2>Contact</h2>
         </div>
         <div className="contact">
-          <form>
-            <p>
-              <label htmlFor="name">
-                Name*
-                <span className={nameError ? 'nameError' : 'nameError__hide'}>
-                  Please enter your full name
-                </span>
-              </label>
-            </p>
-            <input id="name" />
+          <form onSubmit={submitForm}>
+            <div className={showResponse ? 'disableForm' : ''}>
+              <p>
+                <label htmlFor="name">
+                  Name<span className="required">* </span>
+                  <span className={nameError ? 'error' : 'error__hide'}>
+                    {' '}
+                    Please enter your name. Full surname is not required
+                  </span>
+                </label>
+              </p>
+              <input
+                autoFocus
+                onChange={handleChange}
+                id="name"
+                type="text"
+                name="name"
+                readOnly={showResponse ? true : false}
+              />
 
-            <p>
-              <label htmlFor="email">
-                Email*<span>Please enter a valid email address</span>
-              </label>
-            </p>
-            <input id="email" />
+              <p>
+                <label htmlFor="email">
+                  Email<span className="required">* </span>
+                  <span className={emailError ? 'error' : 'error__hide'}>
+                    {' '}
+                    Please enter a valid email address
+                  </span>
+                </label>
+              </p>
+              <input
+                onChange={handleChange}
+                id="email"
+                type="text"
+                name="email"
+                readOnly={showResponse ? true : false}
+              />
 
-            <p>
-              <label htmlFor="phone">Phone</label>
-            </p>
-            <input id="phone" />
-            <p>
-              <label htmlFor="message">
-                Message*<span>Requires 20 more characters</span>
-              </label>
-            </p>
-            <textarea id="message"></textarea>
-            <button className="submitButton">
-              <span>Send</span>
-            </button>
+              <p>
+                <label htmlFor="phone">Phone</label>
+              </p>
+              <input
+                onChange={filterPhone}
+                id="phone"
+                type="text"
+                name="phone"
+                readOnly={showResponse ? true : false}
+              />
+
+              <p>
+                <label htmlFor="message">
+                  Message<span className="required">* </span>
+                  <span className={messageError ? 'error' : 'error__hide'}>
+                    {' '}
+                    Requires {count} more characters
+                  </span>
+                </label>
+              </p>
+              <textarea
+                onChange={handleChange}
+                id="message"
+                type="text"
+                name="message"
+                readOnly={showResponse ? true : false}
+              />
+            </div>
+
+            <div className="submittedForm">
+              <button
+                className="submitButton"
+                type="submit"
+                disabled={
+                  nameError || emailError || messageError || showResponse
+                }
+              >
+                <span>Send</span>
+              </button>
+              <div
+                className={showResponse ? 'messageSent' : 'messageSent__hide'}
+              >
+                <input
+                  readOnly
+                  type="text"
+                  value="Message sent successfully!"
+                />
+              </div>
+            </div>
           </form>
         </div>
       </div>
     </>
   );
 }
-
-export default Contact;
